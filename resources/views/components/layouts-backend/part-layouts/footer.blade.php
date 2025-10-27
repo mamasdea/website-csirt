82ad6da19b8c0db90af9ee8a3c09011d046342dc
mamasdea <deaaldy.28@gmail.com>
2025-10-27 09:55:40 +0700

    update template

--- resources/views/components/layouts-backend/part-layouts/footer.blade.php
+++ resources/views/components/layouts-backend/part-layouts/footer.blade.php
@@ -12,21 +12,21 @@
      <!-- REQUIRED SCRIPTS -->
 
      <!-- jQuery -->
-     <script src="{{ asset('AdminLTE-3.2.0/plugins/jquery/jquery.min.js') }}"></script>
+     <script src="{{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
      <!-- Bootstrap -->
-     <script src="{{ asset('AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+     <script src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
      <!-- AdminLTE -->
-     <script src="{{ asset('AdminLTE-3.2.0/dist/js/adminlte.min.js') }}"></script>
+     <script src="{{ asset('template/dist/js/adminlte.min.js') }}"></script>
 
      <!-- OPTIONAL SCRIPTS -->
-     <script src="{{ asset('AdminLTE-3.2.0/plugins/chart.js/Chart.min.js') }}" defer></script>
+     <script src="{{ asset('template/plugins/chart.js/Chart.min.js') }}" defer></script>
 
      <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-     <script src="{{ asset('AdminLTE-3.2.0/dist/js/pages/dashboard3.js') }}" data-navigate></script>
+     <script src="{{ asset('template/dist/js/pages/dashboard3.js') }}" data-navigate></script>
      {{-- @stack('js') --}}
      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
      <!-- Select2 -->
-     <script src="{{ asset('AdminLTE-3.2.0/plugins/select2/js/select2.full.min.js') }}"></script>
+     <script src="{{ asset('template/plugins/select2/js/select2.full.min.js') }}"></script>
 
 
      @livewireScripts
--- resources/views/components/layouts-backend/part-layouts/header.blade.php
+++ resources/views/components/layouts-backend/part-layouts/header.blade.php
@@ -14,17 +14,16 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
 
     <!-- IonIcons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/dist/css/adminlte.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/dist/css/adminlte.css') }}">
     <!-- Select2 -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/plugins/select2/css/select2.min.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('AdminLTE-3.2.0/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
     @vite('resources/css/app.css')
     @livewireStyles
     @stack('css')
